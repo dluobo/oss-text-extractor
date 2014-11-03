ecf53cfb7ab8a88276d1be6f7a6deeed56dc8a79
Emmanuel Keller <ekeller@open-search-server.com>
2014-11-03 20:19:07 +0100

    Implements #5

---
+++ src/main/java/com/opensearchserver/textextractor/parser/Xls.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2014 OpenSearchServer Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.opensearchserver.textextractor.parser;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.poi.hpsf.SummaryInformation;
+import org.apache.poi.hssf.extractor.ExcelExtractor;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import com.opensearchserver.textextractor.ParserAbstract;
+import com.opensearchserver.textextractor.ParserDocument;
+import com.opensearchserver.textextractor.ParserField;
+
+public class Xls extends ParserAbstract {
+
+	final protected static ParserField TITLE = ParserField.newString("title",
+			"The title of the document");
+
+	final protected static ParserField AUTHOR = ParserField.newString("author",
+			"The name of the author");
+
+	final protected static ParserField KEYWORDS = ParserField.newString(
+			"keywords", null);
+
+	final protected static ParserField SUBJECT = ParserField.newString(
+			"subject", "The subject of the document");
+
+	final protected static ParserField CREATION_DATE = ParserField.newDate(
+			"creation_date", null);
+
+	final protected static ParserField MODIFICATION_DATE = ParserField.newDate(
+			"modification_date", null);
+
+	final protected static ParserField CONTENT = ParserField.newString(
+			"content", "The content of the document");
+
+	final protected static ParserField LANG_DETECTION = ParserField.newString(
+			"lang_detection", "Detection of the language");
+
+	final protected static ParserField[] FIELDS = { TITLE, AUTHOR, KEYWORDS,
+			SUBJECT, CREATION_DATE, MODIFICATION_DATE, CONTENT, LANG_DETECTION };
+
+	public Xls() {
+	}
+
+	@Override
+	protected ParserField[] getParameters() {
+		return null;
+	}
+
+	@Override
+	protected ParserField[] getFields() {
+		return FIELDS;
+	}
+
+	@Override
+	protected void parseContent(InputStream inputStream) throws Exception {
+
+		HSSFWorkbook workbook = new HSSFWorkbook(inputStream);
+		ExcelExtractor excel = null;
+		try {
+			excel = new ExcelExtractor(workbook);
+
+			SummaryInformation info = excel.getSummaryInformation();
+			if (info != null) {
+				metas.add(TITLE, info.getTitle());
+				metas.add(AUTHOR, info.getAuthor());
+				metas.add(SUBJECT, info.getSubject());
+				metas.add(CREATION_DATE, info.getCreateDateTime());
+				metas.add(MODIFICATION_DATE, info.getLastSaveDateTime());
+				metas.add(KEYWORDS, info.getKeywords());
+			}
+
+			ParserDocument result = getNewParserDocument();
+			result.add(CONTENT, excel.getText());
+			result.add(LANG_DETECTION, languageDetection(CONTENT, 10000));
+		} finally {
+			if (excel != null)
+				IOUtils.closeQuietly(excel);
+		}
+
+	}
+}
